2c0d875cdf7db8f84e81c4b62eb14e75a7827655
aragon0013 <andreas.h.mueller@bluewin.ch>
2025-10-24 19:47:19 +0200

    added first draft of LightControl

---
+++ UI/LightControl.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'LightControlYAQMcb.ui'
+##
+## Created by: Qt User Interface Compiler version 6.9.3
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+import sys
+from PyQt6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PyQt6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PyQt6.QtWidgets import (QAbstractButton, QApplication, QCheckBox, QDialog,
+    QDialogButtonBox, QFrame, QGridLayout, QLabel,
+    QLineEdit, QSizePolicy, QWidget)
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(505, 354)
+        self.buttonBox = QDialogButtonBox(Dialog)
+        self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setGeometry(QRect(330, 320, 161, 32))
+        self.buttonBox.setOrientation(Qt.Orientation.Horizontal)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Ok)
+        self.gridLayoutWidget = QWidget(Dialog)
+        self.gridLayoutWidget.setObjectName(u"gridLayoutWidget")
+        self.gridLayoutWidget.setGeometry(QRect(80, 60, 344, 230))
+        self.gridLayout = QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.lineEdit_7 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_7.setObjectName(u"lineEdit_7")
+
+        self.gridLayout.addWidget(self.lineEdit_7, 4, 3, 1, 1)
+
+        self.label_3 = QLabel(self.gridLayoutWidget)
+        self.label_3.setObjectName(u"label_3")
+
+        self.gridLayout.addWidget(self.label_3, 5, 0, 1, 1)
+
+        self.lineEdit_11 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_11.setObjectName(u"lineEdit_11")
+
+        self.gridLayout.addWidget(self.lineEdit_11, 9, 2, 1, 1)
+
+        self.lineEdit_9 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_9.setObjectName(u"lineEdit_9")
+
+        self.gridLayout.addWidget(self.lineEdit_9, 6, 3, 1, 1)
+
+        self.line_2 = QFrame(self.gridLayoutWidget)
+        self.line_2.setObjectName(u"line_2")
+        self.line_2.setFrameShape(QFrame.Shape.HLine)
+        self.line_2.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.gridLayout.addWidget(self.line_2, 0, 2, 1, 1)
+
+        self.label = QLabel(self.gridLayoutWidget)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout.addWidget(self.label, 4, 0, 1, 1)
+
+        self.label_7 = QLabel(self.gridLayoutWidget)
+        self.label_7.setObjectName(u"label_7")
+
+        self.gridLayout.addWidget(self.label_7, 2, 2, 1, 1)
+
+        self.checkBox = QCheckBox(self.gridLayoutWidget)
+        self.checkBox.setObjectName(u"checkBox")
+
+        self.gridLayout.addWidget(self.checkBox, 1, 2, 1, 2)
+
+        self.lineEdit_6 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_6.setObjectName(u"lineEdit_6")
+
+        self.gridLayout.addWidget(self.lineEdit_6, 3, 3, 1, 1)
+
+        self.lineEdit_12 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_12.setObjectName(u"lineEdit_12")
+
+        self.gridLayout.addWidget(self.lineEdit_12, 9, 3, 1, 1)
+
+        self.lineEdit_2 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+
+        self.gridLayout.addWidget(self.lineEdit_2, 4, 2, 1, 1)
+
+        self.label_8 = QLabel(self.gridLayoutWidget)
+        self.label_8.setObjectName(u"label_8")
+
+        self.gridLayout.addWidget(self.label_8, 2, 3, 1, 1)
+
+        self.lineEdit_10 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_10.setObjectName(u"lineEdit_10")
+
+        self.gridLayout.addWidget(self.lineEdit_10, 7, 3, 1, 1)
+
+        self.lineEdit_5 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_5.setObjectName(u"lineEdit_5")
+
+        self.gridLayout.addWidget(self.lineEdit_5, 7, 2, 1, 1)
+
+        self.label_6 = QLabel(self.gridLayoutWidget)
+        self.label_6.setObjectName(u"label_6")
+
+        self.gridLayout.addWidget(self.label_6, 9, 0, 1, 1)
+
+        self.lineEdit_3 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+
+        self.gridLayout.addWidget(self.lineEdit_3, 5, 2, 1, 1)
+
+        self.lineEdit_8 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_8.setObjectName(u"lineEdit_8")
+
+        self.gridLayout.addWidget(self.lineEdit_8, 5, 3, 1, 1)
+
+        self.lineEdit_4 = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_4.setObjectName(u"lineEdit_4")
+
+        self.gridLayout.addWidget(self.lineEdit_4, 6, 2, 1, 1)
+
+        self.label_2 = QLabel(self.gridLayoutWidget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.gridLayout.addWidget(self.label_2, 3, 0, 1, 1)
+
+        self.label_4 = QLabel(self.gridLayoutWidget)
+        self.label_4.setObjectName(u"label_4")
+
+        self.gridLayout.addWidget(self.label_4, 6, 0, 1, 1)
+
+        self.label_5 = QLabel(self.gridLayoutWidget)
+        self.label_5.setObjectName(u"label_5")
+
+        self.gridLayout.addWidget(self.label_5, 7, 0, 1, 1)
+
+        self.lineEdit = QLineEdit(self.gridLayoutWidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.gridLayout.addWidget(self.lineEdit, 3, 2, 1, 1)
+
+        self.line = QFrame(self.gridLayoutWidget)
+        self.line.setObjectName(u"line")
+        self.line.setFrameShape(QFrame.Shape.HLine)
+        self.line.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.gridLayout.addWidget(self.line, 8, 0, 1, 1)
+
+        self.checkBox_2 = QCheckBox(self.gridLayoutWidget)
+        self.checkBox_2.setObjectName(u"checkBox_2")
+
+        self.gridLayout.addWidget(self.checkBox_2, 3, 1, 1, 1)
+
+        self.checkBox_3 = QCheckBox(self.gridLayoutWidget)
+        self.checkBox_3.setObjectName(u"checkBox_3")
+
+        self.gridLayout.addWidget(self.checkBox_3, 4, 1, 1, 1)
+
+        self.checkBox_4 = QCheckBox(self.gridLayoutWidget)
+        self.checkBox_4.setObjectName(u"checkBox_4")
+
+        self.gridLayout.addWidget(self.checkBox_4, 5, 1, 1, 1)
+
+        self.checkBox_5 = QCheckBox(self.gridLayoutWidget)
+        self.checkBox_5.setObjectName(u"checkBox_5")
+
+        self.gridLayout.addWidget(self.checkBox_5, 6, 1, 1, 1)
+
+        self.checkBox_6 = QCheckBox(self.gridLayoutWidget)
+        self.checkBox_6.setObjectName(u"checkBox_6")
+
+        self.gridLayout.addWidget(self.checkBox_6, 7, 1, 1, 1)
+
+        self.label_9 = QLabel(self.gridLayoutWidget)
+        self.label_9.setObjectName(u"label_9")
+
+        self.gridLayout.addWidget(self.label_9, 2, 0, 1, 2)
+
+        self.label_10 = QLabel(Dialog)
+        self.label_10.setObjectName(u"label_10")
+        self.label_10.setGeometry(QRect(10, 0, 211, 41))
+        font = QFont()
+        font.setPointSize(18)
+        font.setBold(True)
+        self.label_10.setFont(font)
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.label_3.setText(QCoreApplication.translate("Dialog", u"green (550nm)", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"blue (550 nm)", None))
+        self.label_7.setText(QCoreApplication.translate("Dialog", u"day", None))
+        self.checkBox.setText(QCoreApplication.translate("Dialog", u"individual D/N Cycles", None))
+        self.label_8.setText(QCoreApplication.translate("Dialog", u"night", None))
+        self.label_6.setText(QCoreApplication.translate("Dialog", u"overall settings", None))
+        self.label_2.setText(QCoreApplication.translate("Dialog", u"white (400nm-750nm)", None))
+        self.label_4.setText(QCoreApplication.translate("Dialog", u"yellow (590 nm)", None))
+        self.label_5.setText(QCoreApplication.translate("Dialog", u"red (660 nm)", None))
+        self.checkBox_2.setText("")
+        self.checkBox_3.setText("")
+        self.checkBox_4.setText("")
+        self.checkBox_5.setText("")
+        self.checkBox_6.setText("")
+        self.label_9.setText(QCoreApplication.translate("Dialog", u"activated LEDs", None))
+        self.label_10.setText(QCoreApplication.translate("Dialog", u"LED Light Control", None))
+    # retranslateUi
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setOrganizationName("Algea PBR Team")
+    app.setApplicationName("PBR OS v.1.0")
+    form = Ui_Dialog()
+    form.show()
+    app.exec()
